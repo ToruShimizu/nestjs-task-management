cfec7611652c0f40e01989f796da1f5f2b59d1de
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-11 17:03:11 +0900

    Add: 任意のタスクを一件取得 する時に、該当のタスクがない場合の処理を追加

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -12,9 +12,13 @@ export class TasksService {
 	getAllTasks() {
 		return this.tasks;
 	}
-	//** タスクidを取得 */
+	//** 任意のタスクを一件取得 */
 	getTaskById(id: string): Task {
-		return this.tasks.find((task) => task.id === id);
+		// 取得対象のタスク
+		const found = this.tasks.find((task) => task.id === id);
+		// 取得対象のタスクがない
+		if (!found) throw new NotFoundException(`Task with ID "${id}" not found`);
+		return found;
 	}
 
 	//** タスク作成 */
