d66fc4ceb73dea28288b671be9fe4d110b17375c
Toru Shimizu <nobbyz.t.nn@gmail.com>
2021-02-11 11:15:29 +0900

    Add: タスク作成機能を追加

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
 	private tasks: Task[] = [];
 
+	//** 全てのタスクの取得 */
 	getAllTasks() {
 		return this.tasks;
+	}
+
+	//** タスク作成 */
+	createTask(title: string, description: string) {
+		const task: Task = {
+			id: uuid(),
+			title,
+			description,
+			status: TaskStatus.OPEN,
+		};
+		this.tasks.push(task);
+		return task;
 	}
 }
